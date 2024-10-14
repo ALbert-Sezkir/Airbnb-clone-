e6553e69f6801f55110f01150acae638abb57a8e
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-14 15:16:57 +0200

    added logo and navbar

--- wonderwise-project/app/layout.tsx
+++ wonderwise-project/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Nunito } from "next/font/google"; 
+import Navbar from "./components/navbar/navbar";
 
 const font = Nunito (
   {
@@ -35,6 +36,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${font.className} antialiased`}
       >
+        <Navbar/>
+
         {children}
       </body>
     </html>
