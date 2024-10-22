221bb52535433ba48cec60618256a71f5b085899
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-22 11:47:27 +0200

    added ImageUpload

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -36,7 +36,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
             onClick={() => open?.()}
             className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
           >
-            <TbPhotoPlus size={50} />
+            <TbPhotoPlus size={40} />
             <div className="font-semibold text-lg">Click to upload</div>
             {value && (
               <div className="absolut inset-0 w-full h-full">
