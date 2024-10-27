10b446fcafa277f32c6a5fdba7f57be7ab7e5463
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-27 13:00:07 +0100

    fixing buggs

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -1,36 +1,31 @@
-import { NextResponse} from "next/server";
-
-import getCurrentUser from "@/app/actions/getCurrentUsers";
-import prisma from "@/app/libs/prismadb";
+import { NextResponse } from 'next/server';
+import prisma from '@/app/libs/prismadb';
+import getCurrentUser from '@/app/actions/getCurrentUsers';
 
 interface IParams {
-
-    reservationId?: string; 
-
+  reservationId?: string;
 }
 
-export async function DELETE (
-    request: Request,
-    {params}: {params: IParams}
-){
-const currentUser = await getCurrentUser();
-if (!currentUser) {
-    return NextResponse.error;
-}
-const {reservationId} = params;
-if (!reservationId || typeof reservationId !== "string") {
-    throw new Error("Invalid ID");
-}
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
 
-const reservation = await prisma.reservation.deleteMany({
+  const { reservationId } = params;
+  if (!reservationId || typeof reservationId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  const reservation = await prisma.reservation.deleteMany({
     where: {
-        id: reservationId,
-        OR: [
-        {userId: currentUser.id},
-        {listing: {userId: currentUser.id}}
-    ]
-    }
-})
+      id: reservationId,
+      userId: currentUser.id, // Ensure the reservation belongs to the current user
+    },
+  });
 
-return NextResponse.json(reservation);
+  return NextResponse.json({ success: true, reservation });
 }
