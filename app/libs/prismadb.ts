778df0f4d2d6dcca831d5d2dd3b9b2a59244e99f
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-28 06:58:02 +0100

    fixing buggs

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,26 +1,26 @@
-// import { PrismaClient } from "@prisma/client";
-
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-
-// const client = globalThis.prisma || new PrismaClient();
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
-
-// export default client;
-
-import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-export default prisma
+export default client;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare global {
+//   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+// }
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
