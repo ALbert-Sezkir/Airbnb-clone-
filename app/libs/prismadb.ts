cefaba4845885e4e011026df1312288467dde799
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-28 06:37:34 +0100

    fixing buggs

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,10 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// const client = globalThis.prisma || new PrismaClient();
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+// export default client;
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export default client;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
