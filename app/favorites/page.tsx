577cb3f2feba180bc997c9fd4fcce836db9e4b82
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-12-02 08:52:52 +0100

    fixing buggs

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,41 +1,71 @@
+// import EmptyState from "../components/EmptyState";
+// import ClientOnly from "../components/ClientOnly";
+
+// import getCurrentUser from "../actions/getCurrentUsers";
+// import getFavoriteListings from "../actions/getFavoritesListings";
+// import FavoritesClient from "./FavoritesClient";
+// import { Suspense } from "react";
+
+
+// const ListingPage = async () => {
+//     const listings = await getFavoriteListings();
+//     const currentUser = await getCurrentUser();
+
+//     if(listings.length === 0) {
+
+//         return (
+            
+//             <ClientOnly>
+//             <EmptyState 
+//             title='No favorites found'
+//             subtitle='Looks like you have no favorites listings'
+//             />
+//         </ClientOnly>
+//     )
+// }
+
+
+// return(
+//     <ClientOnly> 
+//         <FavoritesClient
+//         listings={listings}
+//         currentUser={currentUser}
+//         />    
+//     </ClientOnly>
+// )
+
+
+// }
+
+// export default ListingPage;
+
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 
 import getCurrentUser from "../actions/getCurrentUsers";
 import getFavoriteListings from "../actions/getFavoritesListings";
 import FavoritesClient from "./FavoritesClient";
-import { Suspense } from "react";
-
 
 const ListingPage = async () => {
-    const listings = await getFavoriteListings();
-    const currentUser = await getCurrentUser();
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
 
-    if(listings.length === 0) {
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings"
+        />
+      </ClientOnly>
+    );
+  }
 
-        return (
-            
-            <ClientOnly>
-            <EmptyState 
-            title='No favorites found'
-            subtitle='Looks like you have no favorites listings'
-            />
-        </ClientOnly>
-    )
-}
-
-
-return(
-    <ClientOnly> 
-        <FavoritesClient
-        listings={listings}
-        currentUser={currentUser}
-        />    
+  return (
+    <ClientOnly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
     </ClientOnly>
-)
-
-
-}
+  );
+};
 
 export default ListingPage;
-
